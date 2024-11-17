253bcca33ea67c23691c01ac55821e5fd6ab793f
asseternity <nakupovasset@gmail.com>
2024-11-17 22:48:33 +0400

    feat: publish to github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, HashRouter } from 'react-router-dom';
 import './index.css';
 import { routes } from './routes';
 
