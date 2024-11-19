183bf97e94e0b559506d2c3a92ef66b161ab357b
asseternity <nakupovasset@gmail.com>
2024-11-19 23:06:25 +0400

    feat: list messages upon loading with mapping and useEffect

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -4,34 +4,35 @@ import { Link } from 'react-router-dom';
 const Newsletter = ({ user }) => {
   const [messages, setMessages] = useState([]);
 
-  const fetchMessages = async () => {
-    console.log('user in React = ');
-    console.log(user);
-    try {
-      const response = await fetch(
-        'https://c5-personal-production.up.railway.app/api/allMessages',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include',
-          body: JSON.stringify({ user }),
+  useEffect(() => {
+    const fetchMessages = async () => {
+      try {
+        const response = await fetch(
+          'https://c5-personal-production.up.railway.app/api/allMessages',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            body: JSON.stringify({ user }),
+          }
+        );
+        if (response.ok) {
+          const data = await response.json();
+          setMessages(data);
         }
-      );
-      if (response.ok) {
-        const data = await response.json();
-        setMessages(data);
+      } catch (err) {
+        console.error('Error during fetching your messages: ', err);
       }
-    } catch (err) {
-      console.error('Error during fetching your messages: ', err);
-    }
-  };
+    };
+
+    fetchMessages();
+  }, [user]);
 
   return (
     <div>
       <h1>Newsletter for {user.username}</h1>
-      <button onClick={fetchMessages}>Fetch messages</button>
       <ul>
         {messages.map((message) => (
           <li key={message.id}>
